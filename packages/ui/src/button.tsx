7c8b01cb52a1ca872aafb1ee592832d6385c0b25
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2024-01-29 21:14:18 +0000

    refactor: fix ui button style formatting

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -11,13 +11,13 @@ const buttonVariants = cva(
     variants: {
       variant: {
         primary:
-          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+          "hover:bg-primary/90 bg-primary text-primary-foreground shadow",
         destructive:
-          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
+          "hover:bg-destructive/90 bg-destructive text-destructive-foreground shadow-sm",
         outline:
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
+          "hover:bg-secondary/80 bg-secondary text-secondary-foreground shadow-sm",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
