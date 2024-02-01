e1993a1f57c46da30e5b84d08e993f7b33746472
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2024-02-01 21:33:40 +0000

    fix: create .env iff it does not already exist

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,4 +3,6 @@ pnpm i
 
 # Configure environment variables
 # There is an `.env.example` in the root directory you can use for reference
-cp .env.example .env
+if [ ! -f .env ]; then
+  cp .env.example .env
+fi
