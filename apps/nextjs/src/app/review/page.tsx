1a3be2e49f601ce6204a9dbcedbde78ca76f7d18
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2024-02-05 18:46:22 +0000

    feat: add review and profile pages

---
+++ apps/nextjs/src/app/review/page.tsx
@@ -0,0 +1,11 @@
+export default async function Review() {
+    return (
+        <main className="container h-screen py-16">
+          <div className="flex flex-col items-center justify-center gap-4">
+            <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+              Add <span className="text-primary">Review</span>
+            </h1>
+          </div>
+        </main>
+      );
+}
