2765417c5645f983d5c25a4a6552e7bbba319681
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2024-02-05 20:40:27 +0000

    refactor: apply prettier formatting

--- apps/nextjs/src/app/_components/navigation.tsx
+++ apps/nextjs/src/app/_components/navigation.tsx
@@ -3,42 +3,42 @@
 import Link from "next/link";
 
 import {
-    NavigationMenu,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-    navigationMenuTriggerStyle,
-    HomeIcon,
-    PlusCircledIcon,
-    PersonIcon,
-  } from "@restauwants/ui/navigation-menu";
+  HomeIcon,
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+  PersonIcon,
+  PlusCircledIcon,
+} from "@restauwants/ui/navigation-menu";
 
 export function Navigation() {
-    return (
-        <NavigationMenu className="px-2 py-1 bg-background rounded-2xl">
-            <NavigationMenuList>
-                <NavigationMenuItem>
-                    <Link href="/feed" legacyBehavior passHref>
-                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                            <HomeIcon />
-                        </NavigationMenuLink>
-                    </Link>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <Link href="/review" legacyBehavior passHref>
-                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                            <PlusCircledIcon />
-                        </NavigationMenuLink>
-                    </Link>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <Link href="/profile" legacyBehavior passHref>
-                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                            <PersonIcon />
-                        </NavigationMenuLink>
-                    </Link>
-                </NavigationMenuItem>
-            </NavigationMenuList>
-        </NavigationMenu>
-    );
+  return (
+    <NavigationMenu className="rounded-2xl bg-background px-2 py-1">
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <Link href="/feed" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              <HomeIcon />
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/review" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              <PlusCircledIcon />
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/profile" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              <PersonIcon />
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
 }
--- apps/nextjs/src/app/review/page.tsx
+++ apps/nextjs/src/app/review/page.tsx
@@ -1,14 +1,14 @@
-  import { CreatePostForm } from "../_components/posts";
+import { CreatePostForm } from "../_components/posts";
 
 export default async function Review() {
-    return (
-        <div className="container min-h-screen py-16">
-          <div className="flex flex-col items-center justify-center gap-4">
-            <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-              Add <span className="text-primary">Review</span>
-            </h1>
-          <CreatePostForm />
-          </div>
-        </div>
-      );
+  return (
+    <div className="container min-h-screen py-16">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+          Add <span className="text-primary">Review</span>
+        </h1>
+        <CreatePostForm />
+      </div>
+    </div>
+  );
 }
