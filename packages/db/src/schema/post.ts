9622653e55878a88f180169f05fd8338c47df554
Nebula5102 <nicklausb@shaw.ca>
2024-02-10 01:23:12 -0800

    feat: changed db reviewText -> reviewDescription

--- packages/db/src/schema/post.ts
+++ packages/db/src/schema/post.ts
@@ -13,7 +13,7 @@ import { mySqlTable } from "./_table";
 export const post = mySqlTable("post", {
   id: int("id").autoincrement().primaryKey(),
   restaurantName: varchar("restaurantName", { length: 256 }),
-  reviewText: varchar("reviewText", { length: 256 }),
+  reviewDescription: varchar("reviewText", { length: 256 }),
   stars: int("stars").default(5),
   price: float("price"),
   date: datetime("date"),
