5c798f34d6a71fb71f890bc9e74ecfaf19b5906d
Rebecca Jeon <she4115@gmail.com>
2024-02-11 04:29:11 -0800

    feat: few edits on createpostschema

--- apps/nextjs/src/app/review/page.tsx
+++ apps/nextjs/src/app/review/page.tsx
@@ -2,11 +2,15 @@ import { CreatePostForm } from "../_components/posts";
 
 export default async function Review() {
   return (
-    <div className="container min-h-screen py-16">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Add <span className="text-primary">Review</span>
-        </h1>
+    <div className="container min-h-screen py-5">
+      <div className = "flex justify-between items-center mb-10">
+        <h6>cancel</h6>
+        <h6>save</h6>
+      </div>
+      <div className="flex flex-col gap-3">
+        <div className = "text-left">
+          <h5>I ate at...</h5>
+        </div>
         <CreatePostForm />
       </div>
     </div>
