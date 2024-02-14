925b7bcd75425260a5e254ff3d638fbb18797f65
camrohwer <camrohwer@gmail.com>
2024-02-14 01:43:13 +0000

    fix: profile page layout changes, username

--- apps/nextjs/src/app/_components/auth-showcase.tsx
+++ apps/nextjs/src/app/_components/auth-showcase.tsx
@@ -8,7 +8,7 @@ export async function AuthShowcase() {
     return (
       <form>
         <Button
-          size="lg"
+          size="sm"
           formAction={async () => {
             "use server";
             await signIn("discord");
@@ -22,10 +22,6 @@ export async function AuthShowcase() {
 
   return (
     <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl">
-        {session && <span>Logged in as {session.user.name}</span>}
-      </p>
-
       <form>
         <Button
           size="lg"
