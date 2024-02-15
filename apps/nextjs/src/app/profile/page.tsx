86c2225d4bc67dcfa802249709ea9bf5ffe953a1
camrohwer <camrohwer@gmail.com>
2024-02-15 05:40:54 +0000

    fix: fixed hydration error, colours

--- apps/nextjs/src/app/profile/page.tsx
+++ apps/nextjs/src/app/profile/page.tsx
@@ -1,4 +1,4 @@
-import { Suspense, useEffect, useState } from "react";
+import { Suspense } from "react";
 import { auth } from "@restauwants/auth";
 import { AuthShowcase } from "../_components/auth-showcase";
 import { api } from "~/trpc/server";
@@ -21,45 +21,22 @@ export async function getUserID() {
 }
 
 
-export default function Profile() {
-  const [userId, setUserId] = useState("");
+export default async function Profile() {
+  const userId = await getUserID();
   const posts = api.post.all();
-
-  useEffect(() => {
-    const fetchUserID = async () => {
-      try {
-        // Check if window is defined to ensure it's executed on the client side
-        if (typeof window !== 'undefined') {
-          const session = await auth();
-
-          if (session && session.user) {
-            const userId = session.user.name;
-            setUserId(userId ?? "");
-          } else {
-            throw new Error("User not authenticated");
-          }
-        }
-      } catch (error) {
-        console.error("Error fetching user ID:", error);
-        setUserId(" ");
-      }
-    };
-
-    void fetchUserID();
-  }, []); // empty dependency array means this effect runs once on mount
 
   return (
     <div className="flex h-screen flex-col">
-      <div className="relative h-1/4 flex-none bg-gray-100 p-4">
+      <div className="relative h-1/4 flex-none rounded-lg bg-muted p-4">
         <div className="absolute bottom-4 left-4">
-          <p className="text-xl text-black ">{userId}</p>
+          <p className="text-2xl font-bold text-primary ">{userId}</p>
         </div>
 
         <div className="absolute right-4 top-4 p-4">
           <AuthShowcase />
         </div>
       </div>
-      <div className="flex-grow bg-white p-4">
+      <div className="flex-grow p-4">
         <Suspense fallback={<h4>Loading...</h4>}>
           <PostList posts={posts} />
         </Suspense>
