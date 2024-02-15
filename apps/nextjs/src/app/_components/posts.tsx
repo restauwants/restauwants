c2e63f3a63fb3449b42ed62b33eea9397fec955f
Nebula5102 <nicklausb@shaw.ca>
2024-02-15 13:49:35 -0800

    feat: created functionality of review page

--- apps/nextjs/src/app/_components/posts.tsx
+++ apps/nextjs/src/app/_components/posts.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { use } from "react";
+import { use, useState } from "react";
 
 import type { RouterOutputs } from "@restauwants/api";
 import { cn } from "@restauwants/ui";
@@ -20,19 +20,20 @@ import { CreatePostSchema } from "@restauwants/validators";
 import { api } from "~/trpc/react";
 
 export function CreatePostForm() {
+  const defaults = {
+    restaurantName: "",
+    reviewDescription: "",
+    stars: 3,
+    price: 0.0,
+    date: new Date(),
+    displayName: "Noodles",
+    username: "",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
   const form = useForm({
     schema: CreatePostSchema,
-    defaultValues: {
-      restaurantName: "",
-      reviewDescription: "",
-      stars: 3,
-      price: 0.0,
-      date: new Date(),
-      displayName: "Noodles",
-      username: "",
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    },
+    defaultValues: defaults,
   });
 
   const utils = api.useUtils();
@@ -49,6 +50,17 @@ export function CreatePostForm() {
       );
     },
   });
+  const [clickedIndex, setClickedIndex] = useState<number>(-1);
+
+  const handleStarClick = (
+    index: number,
+  ): typeof defaults | { stars: number } => {
+    setClickedIndex(index);
+    return {
+      ...form.defaultValues,
+      stars: clickedIndex + 1,
+    } as typeof defaults | { stars: number };
+  };
 
   return (
     <Form {...form}>
@@ -64,7 +76,30 @@ export function CreatePostForm() {
           render={({ field }) => (
             <FormItem>
               <FormControl>
-                <Input {...field} placeholder="Enter the Restaurant" />
+                <Input
+                  onChange={field.onChange}
+                  value={field.value}
+                  name={field.name}
+                  placeholder="Enter the restaurant"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="reviewDescription"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <textarea
+                  onChange={field.onChange}
+                  value={field.value}
+                  name={field.name}
+                  className="h-40 w-full border-2 p-2 text-sm"
+                  placeholder="Leave a review"
+                />
               </FormControl>
               <FormMessage />
             </FormItem>
@@ -73,17 +108,72 @@ export function CreatePostForm() {
         <FormField
           control={form.control}
           name="stars"
-          render={() => <div></div>}
+          render={() => (
+            <div className="z-50 flex gap-0.5">
+              {Array.from({ length: 5 }).map((_, index) => (
+                <svg
+                  key={index}
+                  className="h-4 w-4"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                  onClick={() => handleStarClick(index)}
+                >
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0" />
+                  <g
+                    id="SVGRepo_tracerCarrier"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <g id="SVGRepo_iconCarrier">
+                    <path
+                      fill={
+                        clickedIndex !== null && index <= clickedIndex
+                          ? "#E4A72F"
+                          : "#808080"
+                      }
+                      d="M9.88993214,17.5143551 L5.67925514,19.7706633 L5.58137404,19.8151697 C4.86621614,20.0856574 4.19175783,20.0309364 3.67796741,19.5732527 C3.19576628,19.1437087 3.02114918,18.5272631 3.12106237,17.8182442 L3.82494419,12.9582802 L0.644943731,9.61635601 C0.0942731134,9.05561309 -0.12003139,8.36789942 0.0652371375,7.6475266 C0.270499931,6.8494108 0.944591599,6.38981101 1.91626844,6.23594046 L6.1429793,5.59822482 L8.35612232,1.01301762 C8.69987723,0.366097394 9.23663637,-0.0174170702 9.91131194,0.000608459089 C10.5717106,0.0182525484 11.0939504,0.422542671 11.5040044,1.14356762 L13.64475,5.62965664 L18.3837285,6.28808837 C19.1367847,6.41806637 19.6918258,6.81025062 19.9136547,7.46427289 C20.1349495,8.11672079 19.9243574,8.77699895 19.3567704,9.45065589 L15.916492,12.9606774 L16.6886186,17.8170551 C16.81148,18.6567374 16.6644875,19.3222694 16.1027873,19.7179201 C15.5971351,20.0740917 14.964405,20.0740917 14.2364881,19.8236405 L14.1161887,19.7712214 L9.88993214,17.5143551 Z"
+                    ></path>
+                  </g>
+                </svg>
+              ))}
+            </div>
+          )}
         />
         <FormField
           control={form.control}
           name="price"
-          render={() => <div></div>}
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  onChange={field.onChange}
+                  value={field.value}
+                  name={field.name}
+                  className="text-money font-extrabold"
+                  placeholder="How much you spent"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
         />
         <FormField
           control={form.control}
           name="date"
-          render={() => <div></div>}
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  onChange={field.onChange}
+                  value={field.value}
+                  name={field.name}
+                  type="date"
+                  className="cursor-pointer border p-2"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
         />
         <FormField
           control={form.control}
@@ -94,18 +184,6 @@ export function CreatePostForm() {
           control={form.control}
           name="username"
           render={() => <div></div>}
-        />
-        <FormField
-          control={form.control}
-          name="reviewDescription"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Input {...field} placeholder="Description" />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
         />
         <FormField
           control={form.control}
--- apps/nextjs/src/app/review/page.tsx
+++ apps/nextjs/src/app/review/page.tsx
@@ -3,11 +3,7 @@ import { CreatePostForm } from "../_components/posts";
 export default async function Review() {
   return (
     <div className="container min-h-screen py-5">
-      <div className="mb-10 flex items-center justify-between">
-        <h6>cancel</h6>
-        <h6>save</h6>
-      </div>
-      <div className="flex flex-col gap-3">
+      <div className="mt-7 flex flex-col gap-3">
         <div className="text-left">
           <h5>I ate at...</h5>
         </div>
