476b94fea55421afd870cf15240b1d1b8ecf67ec
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2024-02-16 05:35:40 +0000

    fix: redirect to login from all pages given missing session

--- apps/nextjs/src/middleware.ts
+++ apps/nextjs/src/middleware.ts
@@ -1,18 +1,18 @@
-import { NextRequest, NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
 import { auth } from "@restauwants/auth";
 
 const baseURL = process.env.AUTH_URL;
 
-//import middleware from "next-auth/middleware";
 export const config = {
-  matcher: ["/review", "/feed", "/profile"],
+  matcher: "/((?!api|static|.*\\..*|_next).*)",
 };
 
 export async function middleware(request: NextRequest) {
   const session = await auth();
 
-  if (!session) {
+  if (!session && !request.nextUrl.pathname.startsWith("/login")) {
     return NextResponse.redirect(new URL(baseURL + "/login"));
   } else if (session && request.nextUrl.pathname.startsWith("/login")) {
     return NextResponse.redirect(new URL(baseURL + "/profile"));
