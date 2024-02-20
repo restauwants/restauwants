a30c5d2d1d5fa1d8e77df07e6a198139d388a0cd
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2024-02-20 02:49:38 +0000

    fix: update nextjs app to use new database schema

--- apps/nextjs/src/app/review/page.tsx
+++ apps/nextjs/src/app/review/page.tsx
@@ -4,9 +4,6 @@ export default async function Review() {
   return (
     <div className="container min-h-screen py-5">
       <div className="mt-7 flex flex-col gap-3">
-        <div className="text-left">
-          <h5>I ate at...</h5>
-        </div>
         <CreateReviewForm />
       </div>
     </div>
