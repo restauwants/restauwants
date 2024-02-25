9c53f382a86a4bce3bf6726d4e976e23993dbf25
Rebecca Jeon <she4115@gmail.com>
2024-02-25 03:23:15 -0800

    feat: new format for posts and structured review page

--- apps/nextjs/src/app/_components/posts.tsx
+++ apps/nextjs/src/app/_components/posts.tsx
@@ -25,7 +25,7 @@ export function CreatePostForm() {
     reviewDescription: "",
     stars: 3,
     price: "",
-    date: new Date().toString(),
+    date: new Date().toISOString().split("T")[0],
     displayName: "Noodles",
     username: "",
     createdAt: new Date(),
@@ -63,136 +63,147 @@ export function CreatePostForm() {
   return (
     <Form {...form}>
       <form
-        className="flex w-full max-w-2xl flex-col gap-4"
+        className="flex items-center justify-center"
         onSubmit={form.handleSubmit(async (data) => {
           createPost.mutate(data);
         })}
       >
-        <FormField
-          control={form.control}
-          name="restaurantName"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Input
-                  onChange={field.onChange}
-                  value={field.value}
-                  name={field.name}
-                  placeholder="Enter the restaurant"
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="reviewDescription"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <textarea
-                  onChange={field.onChange}
-                  value={field.value}
-                  name={field.name}
-                  className="h-40 w-full border-2 p-2 text-sm"
-                  placeholder="Leave a review"
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="stars"
-          render={() => (
-            <div className="z-50 flex gap-0.5">
-              {Array.from({ length: 5 }).map((_, index) => (
-                <svg
-                  key={index}
-                  className="h-4 w-4"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                  onClick={() => {
-                    handleStarClick(index);
-                  }}
-                >
-                  <g id="SVGRepo_bgCarrier" strokeWidth="0" />
-                  <g
-                    id="SVGRepo_tracerCarrier"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
+        <div className="flex w-full max-w-2xl flex-col gap-4 p-5 font-mono">
+          <div className="text-left">
+            <h6>I ate at...</h6>
+          </div>
+          <FormField
+            control={form.control}
+            name="restaurantName"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    onChange={field.onChange}
+                    value={field.value}
+                    name={field.name}
+                    placeholder="Enter the restaurant"
                   />
-                  <g id="SVGRepo_iconCarrier">
-                    <path
-                      fill={index <= clickedIndex ? "#E4A72F" : "#808080"}
-                      d="M9.88993214,17.5143551 L5.67925514,19.7706633 L5.58137404,19.8151697 C4.86621614,20.0856574 4.19175783,20.0309364 3.67796741,19.5732527 C3.19576628,19.1437087 3.02114918,18.5272631 3.12106237,17.8182442 L3.82494419,12.9582802 L0.644943731,9.61635601 C0.0942731134,9.05561309 -0.12003139,8.36789942 0.0652371375,7.6475266 C0.270499931,6.8494108 0.944591599,6.38981101 1.91626844,6.23594046 L6.1429793,5.59822482 L8.35612232,1.01301762 C8.69987723,0.366097394 9.23663637,-0.0174170702 9.91131194,0.000608459089 C10.5717106,0.0182525484 11.0939504,0.422542671 11.5040044,1.14356762 L13.64475,5.62965664 L18.3837285,6.28808837 C19.1367847,6.41806637 19.6918258,6.81025062 19.9136547,7.46427289 C20.1349495,8.11672079 19.9243574,8.77699895 19.3567704,9.45065589 L15.916492,12.9606774 L16.6886186,17.8170551 C16.81148,18.6567374 16.6644875,19.3222694 16.1027873,19.7179201 C15.5971351,20.0740917 14.964405,20.0740917 14.2364881,19.8236405 L14.1161887,19.7712214 L9.88993214,17.5143551 Z"
-                    ></path>
-                  </g>
-                </svg>
-              ))}
-            </div>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="price"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Input
-                  type="number"
-                  onChange={field.onChange}
-                  value={field.value}
-                  name={field.name}
-                  className="text-money font-extrabold"
-                  placeholder="How much you spent"
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="date"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Input
-                  onChange={field.onChange}
-                  value={field.value}
-                  name={field.name}
-                  type="date"
-                  className="cursor-pointer border p-2"
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="displayName"
-          render={() => <div></div>}
-        />
-        <FormField
-          control={form.control}
-          name="username"
-          render={() => <div></div>}
-        />
-        <FormField
-          control={form.control}
-          name="createdAt"
-          render={() => <div></div>}
-        />
-        <FormField
-          control={form.control}
-          name="updatedAt"
-          render={() => <div></div>}
-        />
-        <Button>Create</Button>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="text-left">
+            <h6>overall rating</h6>
+          </div>
+          <FormField
+            control={form.control}
+            name="stars"
+            render={() => (
+              <div className="z-50 flex gap-5">
+                {Array.from({ length: 5 }).map((_, index) => (
+                  <svg
+                    key={index}
+                    className="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                    onClick={() => {
+                      handleStarClick(index);
+                    }}
+                  >
+                    <g id="SVGRepo_bgCarrier" strokeWidth="0" />
+                    <g
+                      id="SVGRepo_tracerCarrier"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                    <g id="SVGRepo_iconCarrier">
+                      <path
+                        fill={index <= clickedIndex ? "#E4A72F" : "#808080"}
+                        d="M9.88993214,17.5143551 L5.67925514,19.7706633 L5.58137404,19.8151697 C4.86621614,20.0856574 4.19175783,20.0309364 3.67796741,19.5732527 C3.19576628,19.1437087 3.02114918,18.5272631 3.12106237,17.8182442 L3.82494419,12.9582802 L0.644943731,9.61635601 C0.0942731134,9.05561309 -0.12003139,8.36789942 0.0652371375,7.6475266 C0.270499931,6.8494108 0.944591599,6.38981101 1.91626844,6.23594046 L6.1429793,5.59822482 L8.35612232,1.01301762 C8.69987723,0.366097394 9.23663637,-0.0174170702 9.91131194,0.000608459089 C10.5717106,0.0182525484 11.0939504,0.422542671 11.5040044,1.14356762 L13.64475,5.62965664 L18.3837285,6.28808837 C19.1367847,6.41806637 19.6918258,6.81025062 19.9136547,7.46427289 C20.1349495,8.11672079 19.9243574,8.77699895 19.3567704,9.45065589 L15.916492,12.9606774 L16.6886186,17.8170551 C16.81148,18.6567374 16.6644875,19.3222694 16.1027873,19.7179201 C15.5971351,20.0740917 14.964405,20.0740917 14.2364881,19.8236405 L14.1161887,19.7712214 L9.88993214,17.5143551 Z"
+                      ></path>
+                    </g>
+                  </svg>
+                ))}
+              </div>
+            )}
+          />
+          <div className="text-left">
+            <h6>cost</h6>
+          </div>
+          <FormField
+            control={form.control}
+            name="price"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    type="number"
+                    onChange={field.onChange}
+                    value={field.value}
+                    name={field.name}
+                    className="text-money"
+                    placeholder="How much you spent"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="text-left">
+            <h5>date</h5>
+          </div>
+          <FormField
+            control={form.control}
+            name="date"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input {...field} type="date" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="text-left">
+            <h5>Share your story!</h5>
+          </div>
+          <FormField
+            control={form.control}
+            name="reviewDescription"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <textarea
+                    onChange={field.onChange}
+                    value={field.value}
+                    name={field.name}
+                    className="h-40 w-full border-2 p-2 text-sm"
+                    placeholder="Leave a review"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="displayName"
+            render={() => <div></div>}
+          />
+          <FormField
+            control={form.control}
+            name="username"
+            render={() => <div></div>}
+          />
+          <FormField
+            control={form.control}
+            name="createdAt"
+            render={() => <div></div>}
+          />
+          <FormField
+            control={form.control}
+            name="updatedAt"
+            render={() => <div></div>}
+          />
+          <Button>Create</Button>
+        </div>
       </form>
     </Form>
   );
@@ -221,7 +232,7 @@ export function PostList(props: {
   }
 
   return (
-    <div className="flex h-full w-full flex-col">
+    <div className="flex h-full w-full flex-col space-y-4">
       {posts.map((p) => {
         return <PostCard key={p.id} post={p} />;
       })}
@@ -246,84 +257,76 @@ export function PostCard(props: {
     },
   });
 
-  const formattedDate = props.post.date.toLocaleString();
-
-  const regex = /^(\d{4})-(\d{2})-(\d{2})$/;
-  const match = formattedDate.match(regex);
-  const [, year, month, day] = match;
+  const temp_url = "https://www." + props.post.restaurantName + ".com";
+  // const logrName = CheckUrl(props);
+  // logrName();
 
   return (
-    <div className="border-base h-full w-full border-b-2">
-      <div className="flex h-7 w-full items-center justify-between text-center">
-        <h2 className="text-xl font-bold">{props.post.restaurantName}</h2>
+    <div className="relative box-border flex h-full w-full flex-col rounded-md border-2 p-2 font-mono">
+      <div className="flex items-center justify-between border-b-2 pb-2">
+        <h2 className="font-mono text-sm font-medium">
+          {props.post.displayName}
+        </h2>
         <div className="flex">
-          <h2 className="text-base font-bold">{props.post.displayName}</h2>
-        </div>
-      </div>
-      <div className="flex h-5 w-full justify-between">
-        <div className="flex h-full w-full justify-start gap-0.5">
-          {Array.from({ length: props.post.stars ?? 5 }).map((_, index) => (
-            <svg
-              key={index}
-              className="h-3 w-3"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g id="SVGRepo_bgCarrier" strokeWidth="0" />
-              <g
-                id="SVGRepo_tracerCarrier"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <g id="SVGRepo_iconCarrier">
-                <path
-                  fill="#E4A72F"
-                  d="M9.88993214,17.5143551 L5.67925514,19.7706633 L5.58137404,19.8151697 C4.86621614,20.0856574 4.19175783,20.0309364 3.67796741,19.5732527 C3.19576628,19.1437087 3.02114918,18.5272631 3.12106237,17.8182442 L3.82494419,12.9582802 L0.644943731,9.61635601 C0.0942731134,9.05561309 -0.12003139,8.36789942 0.0652371375,7.6475266 C0.270499931,6.8494108 0.944591599,6.38981101 1.91626844,6.23594046 L6.1429793,5.59822482 L8.35612232,1.01301762 C8.69987723,0.366097394 9.23663637,-0.0174170702 9.91131194,0.000608459089 C10.5717106,0.0182525484 11.0939504,0.422542671 11.5040044,1.14356762 L13.64475,5.62965664 L18.3837285,6.28808837 C19.1367847,6.41806637 19.6918258,6.81025062 19.9136547,7.46427289 C20.1349495,8.11672079 19.9243574,8.77699895 19.3567704,9.45065589 L15.916492,12.9606774 L16.6886186,17.8170551 C16.81148,18.6567374 16.6644875,19.3222694 16.1027873,19.7179201 C15.5971351,20.0740917 14.964405,20.0740917 14.2364881,19.8236405 L14.1161887,19.7712214 L9.88993214,17.5143551 Z"
-                ></path>
-              </g>
-            </svg>
-          ))}
-          {Array.from({ length: 5 - (props.post.stars ?? 0) }).map(
-            (_, index) => (
-              <svg
-                key={index}
-                className="h-3 w-3"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g id="SVGRepo_bgCarrier" strokeWidth="0" />
-                <g
-                  id="SVGRepo_tracerCarrier"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-                <g id="SVGRepo_iconCarrier">
-                  <path
-                    fill="#555"
-                    d="M9.88993214,17.5143551 L5.67925514,19.7706633 L5.58137404,19.8151697 C4.86621614,20.0856574 4.19175783,20.0309364 3.67796741,19.5732527 C3.19576628,19.1437087 3.02114918,18.5272631 3.12106237,17.8182442 L3.82494419,12.9582802 L0.644943731,9.61635601 C0.0942731134,9.05561309 -0.12003139,8.36789942 0.0652371375,7.6475266 C0.270499931,6.8494108 0.944591599,6.38981101 1.91626844,6.23594046 L6.1429793,5.59822482 L8.35612232,1.01301762 C8.69987723,0.366097394 9.23663637,-0.0174170702 9.91131194,0.000608459089 C10.5717106,0.0182525484 11.0939504,0.422542671 11.5040044,1.14356762 L13.64475,5.62965664 L18.3837285,6.28808837 C19.1367847,6.41806637 19.6918258,6.81025062 19.9136547,7.46427289 C20.1349495,8.11672079 19.9243574,8.77699895 19.3567704,9.45065589 L15.916492,12.9606774 L16.6886186,17.8170551 C16.81148,18.6567374 16.6644875,19.3222694 16.1027873,19.7179201 C15.5971351,20.0740917 14.964405,20.0740917 14.2364881,19.8236405 L14.1161887,19.7712214 L9.88993214,17.5143551 Z"
-                  ></path>
-                </g>
-              </svg>
-            ),
+          {props.post.date && (
+            <p className="font-mono text-sm font-medium">
+              {new Date(props.post.date).toLocaleDateString()}
+            </p>
           )}
-          <p className="text-money text-xs">${props.post.price}</p>
         </div>
       </div>
-      <div className="flex w-full justify-start">
-        <p className="text-xs">
-          {day} {month}, {year}
-        </p>
+      <div className="flex flex-grow flex-col py-2">
+        <h2 className="font-mono text-2xl font-bold">
+          <a href={temp_url}>{props.post.restaurantName}</a>
+        </h2>
+        <div className="text-sm">{props.post.reviewDescription}</div>
+        <div className="mt-3 flex items-center gap-1">
+          <svg
+            className="h-6 w-6"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g id="SVGRepo_bgCarrier" strokeWidth="0" />
+            <g
+              id="SVGRepo_tracerCarrier"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <g id="SVGRepo_iconCarrier">
+              <path
+                fill="#E4A72F"
+                d="M9.5 14.25l-5.584 2.936 1.066-6.218L.465 6.564l6.243-.907L9.5 0l2.792 5.657 6.243.907-4.517 4.404 1.066 6.218"
+              ></path>
+            </g>
+          </svg>
+          <div className="text-3xl font-semibold "> {props.post.stars}</div>
+          <div className="text-money ml-auto flex items-center gap-1">
+            ${props.post.price}
+          </div>
+        </div>
+        <div className="flex justify-end">
+          <Button
+            className="h-3 cursor-pointer text-sm font-bold hover:bg-transparent hover:text-white"
+            onClick={() => deletePost.mutate(props.post.id)}
+          >
+            Delete
+          </Button>
+        </div>
       </div>
-      <div className="flex-grow text-sm">{props.post.reviewDescription}</div>
-      <Button
-        className="h-3 cursor-pointer text-sm font-bold hover:bg-transparent hover:text-white"
-        onClick={() => deletePost.mutate(props.post.id)}
-      >
-        Delete
-      </Button>
     </div>
   );
 }
+
+// export function CheckUrl(props:{
+//   post: RouterOutputs["post"]["all"][number];
+//   }){
+//   const check_c = [".com", ".ca"];
+//   const r_url = props.post.restaurantName;
+//   const url_str = "https://" + r_url;
+//   return(
+
+//   );
+// }
 
 export function PostCardSkeleton(props: { pulse?: boolean }) {
   const { pulse = true } = props;
