17e2c9e7f9ed45c1f9c1a0027373754eee164692
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2024-03-03 22:25:07 +0000

    refactor: inline loginButton component

--- apps/nextjs/src/app/(registration)/login/page.tsx
+++ apps/nextjs/src/app/(registration)/login/page.tsx
@@ -1,10 +1,16 @@
-import { LoginButton } from "../../../components/loginButton";
+import { Button } from "@restauwants/ui/button";
+
+import { login } from "../../actions";
 
 export default async function Login() {
   return (
     <>
       <p>Please login to continue</p>
-      <LoginButton />
+      <form>
+        <Button size="sm" formAction={login}>
+          Sign in with Discord
+        </Button>
+      </form>
     </>
   );
 }
