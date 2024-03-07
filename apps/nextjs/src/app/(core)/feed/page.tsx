3a2d726c0dbbcca952f7e53ffe69b9b419400ea4
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2024-03-07 06:08:39 +0000

    feat: improve review card styling

--- apps/nextjs/src/app/(core)/feed/page.tsx
+++ apps/nextjs/src/app/(core)/feed/page.tsx
@@ -10,7 +10,7 @@ export default async function Feed() {
   const reviews = api.review.all();
 
   return (
-    <div className="container min-h-dvh py-16">
+    <div className="container min-h-dvh pb-20 pt-12">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
           Restau<span className="text-primary">Wants</span>
--- apps/nextjs/src/app/(core)/profile/page.tsx
+++ apps/nextjs/src/app/(core)/profile/page.tsx
@@ -21,7 +21,7 @@ function More() {
     <Dialog>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <Button variant="outline" size="icon">
+          <Button variant="ghost" size="icon">
             <DotsVerticalIcon className="size-5" />
           </Button>
         </DropdownMenuTrigger>
@@ -43,14 +43,14 @@ export default async function Profile() {
   const reviews = api.review.all();
 
   return (
-    <div className="min-h-dvh w-full max-w-2xl space-y-4 divide-y-2">
+    <div className="min-h-dvh w-full max-w-2xl space-y-4 divide-y-4 pb-20 pt-12">
       <div className="flex flex-row items-start justify-between gap-5 px-4 pt-4">
         <p className="min-w-0 break-words text-2xl font-bold text-primary">
           {username}
         </p>
         <More />
       </div>
-      <div className="container">
+      <div className="container pt-4">
         <Suspense fallback={<h4>Loading...</h4>}>
           <ReviewList reviews={reviews} />
         </Suspense>
