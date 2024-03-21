b5e234fa247bf03de8f30121ca944a05603f6f41
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2024-03-21 04:43:07 +0000

    fix: fix external collection schema

--- packages/validators/src/server/external/index.ts
+++ packages/validators/src/server/external/index.ts
@@ -64,26 +64,11 @@ export const CreateProfileSchema = z.object({
 });
 
 export const CreateCollectionSchema = z.object({
-  userId: z.string().min(0).max(255),
   name: z.string().min(0).max(255),
   description: z.string().min(0).max(255),
-  createdAt: z.date().refine((d) => d <= new Date(), {
-    message: "createdAt must be in the past",
-  }),
 });
 
 export const AddRestaurantToCollectionSchema = z.object({
-  collectionId: z.number().int(),
+  collectionId: z.number().int().positive(),
   restaurantId: z.string().min(0).max(255),
-  createdAt: z.date().refine((d) => d <= new Date(), {
-    message: "createdAt must be in the past",
-  }),
-});
-
-export const AddRestaurantSchema = z.object({
-  id: z.string(),
-  name: z.string().min(0).max(255),
-  website: z.string().min(0).max(255),
-  formatted_phone_number: z.string().min(0).max(255),
-  formatted_address: z.string().min(0).max(255),
 });
