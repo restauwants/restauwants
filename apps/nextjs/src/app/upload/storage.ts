c1b0e1b5f3a462e4809e5a1d266fbda52c293902
Norman Anderson <147473940+anormananderson@users.noreply.github.com>
2024-03-27 07:48:56 +0000

    fix: remove @google-cloud/pubsub because it was unused and causing additional issues

--- apps/nextjs/src/app/upload/storage.ts
+++ apps/nextjs/src/app/upload/storage.ts
@@ -1,22 +1,9 @@
 "use server";
 
-import { PubSub } from "@google-cloud/pubsub";
 import { Storage } from "@google-cloud/storage";
 
 export async function getStorage() {
   return new Storage({
-    projectId: "nifty-edge-415820",
-    credentials: {
-      client_email: process.env.GCS_CLIENT_EMAIL,
-      private_key: process.env.GCS_PRIVATE_KEY?.split(String.raw`\n`).join(
-        "\n",
-      ),
-    },
-  });
-}
-
-export async function getPubSubClient() {
-  return new PubSub({
     projectId: "nifty-edge-415820",
     credentials: {
       client_email: process.env.GCS_CLIENT_EMAIL,
