de468069b644445e2b8885e6cbfc94fd7adf4bfb
Cam Rohwer <crohwer@uvic.ca>
2024-03-27 22:01:32 +0000

    fix: fix to dependencies, icons

--- packages/ui/src/icons.tsx
+++ packages/ui/src/icons.tsx
@@ -1,15 +1,7 @@
-import {
+export {
   CalendarIcon,
   DollarSignIcon,
   MoreVerticalIcon as DotsVerticalIcon,
   SettingsIcon,
   StarIcon,
 } from "lucide-react";
-
-export {
-  SettingsIcon,
-  DotsVerticalIcon,
-  StarIcon,
-  CalendarIcon,
-  DollarSignIcon,
-};
